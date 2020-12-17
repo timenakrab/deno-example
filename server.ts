4f52c0adb7856d15085a7a0eb4fb2f5790ab7063
Streetboy <timenakrab@gmail.com>
2020-12-17 19:58:27 +0700

    setup env

--- server.ts
+++ server.ts
@@ -1,12 +1,14 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts';
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-import router from './route.ts';
-const port = 5000;
+import router from "./route.ts";
 
 const app = new Application();
+const { PORT } = config({ safe: true });
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`Server running on port ${port}`);
+console.log(`Server running on port ${PORT}`);
 
-app.listen({ port });
+app.listen({ port: Number(PORT) });
