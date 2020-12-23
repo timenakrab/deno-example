5271c51d344831be442e01b7a2dbbc87a25614ff
watchara <watchara@punpromotion.com>
2020-12-23 14:08:38 +0700

    import RouterContext

--- route.ts
+++ route.ts
@@ -1,16 +1,16 @@
-import { Router } from 'https://deno.land/x/oak/mod.ts';
+import { Router, RouterContext } from "https://deno.land/x/oak/mod.ts";
 
 const router = new Router();
 
-router.get('/', ({ response }) => {
-  response.headers.set('content-type', 'application/json');
+router.get("/", ({ response }: RouterContext) => {
+  response.headers.set("content-type", "application/json");
   response.body = JSON.stringify({
-    text: 'Hello, Moon',
+    text: "Hello, Moon",
   });
 });
 
-router.get('/api/products', ({ response }: { response: any }) => {
-  response.body = 'hello';
+router.get("/api/products", ({ response }: RouterContext) => {
+  response.body = "hello";
 });
 
 export default router;
